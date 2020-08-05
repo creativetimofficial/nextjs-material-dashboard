d03c2672db52b96624a4a047face3ad792457c66
EINazare <emanuelioannazare@gmail.com>
2020-08-05 18:44:00 +0300

    fix: solve chartist issues

--- pages/_document.js
+++ pages/_document.js
@@ -22,12 +22,20 @@ class MyDocument extends Document {
           {/* Fonts and icons */}
           <link
             rel="stylesheet"
-            type="text/css"
-            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons"
+            href="//cdn.jsdelivr.net/chartist.js/latest/chartist.min.css"
+          />
+          <script src="//cdn.jsdelivr.net/chartist.js/latest/chartist.min.js"></script>
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons"
           />
           <link
-            href="https://use.fontawesome.com/releases/v5.0.10/css/all.css"
+            href="https://fonts.googleapis.com/icon?family=Material+Icons"
             rel="stylesheet"
+          />
+          <link
+            rel="stylesheet"
+            href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
           />
         </Head>
         <body>
