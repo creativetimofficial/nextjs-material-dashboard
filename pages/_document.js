862517947054f638c04d9e210c690f1e63b841ea
EINazare <emanuelioannazare@gmail.com>
2020-08-06 17:08:41 +0300

    ready to deploy

--- pages/_document.js
+++ pages/_document.js
@@ -15,11 +15,11 @@ class MyDocument extends Document {
           ></style>
           <script
             dangerouslySetInnerHTML={{
-              __html: `(function(a,s,y,n,c,h,i,d,e){s.className+=' '+y;h.start=1*new Date;
-              h.end=i=function(){s.className=s.className.replace(RegExp(' ?'+y),'')};
-              (a[n]=a[n]||[]).hide=h;setTimeout(function(){i();h.end=null},c);h.timeout=c;
-              })(window,document.documentElement,'async-hide','dataLayer',4000,
-              {'GTM-K9BGS8K':true});`,
+__html: `(function(a,s,y,n,c,h,i,d,e){s.className+=' '+y;h.start=1*new Date;
+h.end=i=function(){s.className=s.className.replace(RegExp(' ?'+y),'')};
+(a[n]=a[n]||[]).hide=h;setTimeout(function(){i();h.end=null},c);h.timeout=c;
+})(window,document.documentElement,'async-hide','dataLayer',4000,
+{'GTM-K9BGS8K':true});`,
             }}
           ></script>
           {/* Analytics-Optimize Snippet */}
