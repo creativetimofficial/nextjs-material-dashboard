90892b9f98c1bdde0f7dd474ee0f621cfd791944
EINazare <emanuelioannazare@gmail.com>
2021-01-29 14:18:10 +0200

    refactor: solve #2 from pro version

--- components/Typography/Info.js
+++ components/Typography/Info.js
@@ -5,9 +5,8 @@ import { makeStyles } from "@material-ui/core/styles";
 // core components
 import styles from "assets/jss/nextjs-material-dashboard/components/typographyStyle.js";
 
-const useStyles = makeStyles(styles);
-
 export default function Info(props) {
+  const useStyles = makeStyles(styles);
   const classes = useStyles();
   const { children } = props;
   return (
